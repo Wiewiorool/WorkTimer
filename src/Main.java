e92a5d217ec5c90e422c5ecf29d04f20f4add664
Wiewior <wasisnkipatryk@gmail.com>
2024-09-21 23:49:25 +0200

    Deleted function to create next month txt, cleared code.

--- src/Main.java
+++ src/Main.java
@@ -1,8 +1,8 @@
 import java.io.*;
 import java.time.LocalDateTime;
 import java.time.Month;
+import java.time.YearMonth;
 import java.time.format.DateTimeFormatter;
-import java.util.Arrays;
 import java.util.Calendar;
 import java.util.Locale;
 import java.util.Scanner;
@@ -11,46 +11,40 @@ public class Main {
     public static void main(String[] args) throws IOException {
         var scanner = new Scanner(System.in);
         int time = scanner.nextInt();
-        DateTimeFormatter dtf = DateTimeFormatter.ofPattern("dd.MM.yyyy");
+        DateTimeFormatter dtf = DateTimeFormatter.ofPattern("dd/MM/yyyy");
         LocalDateTime now = LocalDateTime.now();
+        Calendar c = Calendar.getInstance();
+        String month = c.getDisplayName(Calendar.MONTH, Calendar.LONG, Locale.ENGLISH);
+        Month month1 = Month.valueOf(month.toUpperCase());
 
-        FileWriter myWriter = new FileWriter("WorkingHours.txt", true);
+        FileWriter myWriter = new FileWriter(month + ".txt", true);
         myWriter.write(dtf.format(now) + " " + Integer.toString(time) + " hours.");
         myWriter.write("\n");
-        myWriter.close();
+        myWriter.flush();
         System.out.println("Successfully wrote to the file.");
 
-        Scanner scanner1 = new Scanner(new File("WorkingHours.txt"));
-
+        var scanner1 = new Scanner(new File(month + ".txt"));
         int sum = 0;
+
         while (scanner1.hasNextLine()) {
             String[] a = scanner1.nextLine().split(" ");
             sum += Integer.parseInt(a[1]);
         }
-        System.out.println(sum);
-        scanner1.close();
-        Calendar c = Calendar.getInstance();
-        String month = c.getDisplayName(Calendar.MONTH, Calendar.LONG, Locale.ENGLISH);
-        Month month1 = Month.valueOf(month.toUpperCase());
-        //System.out.println(month1.length(true));
-
-        String name = month + ".txt";
-        FileWriter monthFile = new FileWriter(name);
-        monthFile.write("Summary of month: " + Integer.toString(sum));
-        monthFile.close();
-
-        Calendar last = Calendar.getInstance();
+       /* Calendar last = Calendar.getInstance();
         last.add(Calendar.MONTH, -1);
         String lastMonth = last.getDisplayName(Calendar.MONTH, Calendar.LONG, Locale.ENGLISH);
-        Month lastMonth1 = Month.valueOf(month.toUpperCase());
+        Month lastMonth1 = Month.valueOf(month.toUpperCase());*/
 
-        BufferedReader reader = new BufferedReader(new FileReader("WorkingHours.txt"));
+        BufferedReader reader = new BufferedReader(new FileReader(month + ".txt"));
 
         int lines = 0;
         while (reader.readLine() != null) lines++;
         reader.close();
-        if (lines != lastMonth1.length(true)) {
-            FileWriter lastMonthFile = new FileWriter(lastMonth + ".txt");
+        if (lines == YearMonth.now().lengthOfMonth()) {
+            myWriter.write("Work time summary of " + month + " " + sum);
+            myWriter.close();
         }
+
     }
+
 }
