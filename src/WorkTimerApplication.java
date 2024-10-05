a03a5b08bfe01cd85add310d95ba3e9178de23a9
Wiewior <wasisnkipatryk@gmail.com>
2024-10-05 22:54:58 +0200

    Refactor code

--- src/WorkTimerApplication.java
+++ src/WorkTimerApplication.java
@@ -1,3 +1,4 @@
+
 import java.io.*;
 import java.time.LocalDateTime;
 import java.time.YearMonth;
@@ -7,6 +8,7 @@ import java.util.Locale;
 import java.util.Scanner;
 
 public class WorkTimerApplication {
+
 
     private static double salaryPerHour = 31.44;
     private static int sum = 0;
@@ -22,29 +24,18 @@ public class WorkTimerApplication {
     private static DateTimeFormatter dtf = DateTimeFormatter.ofPattern("dd/MM/yyyy");
     private static LocalDateTime now = LocalDateTime.now();
 
+
     public static void main(String[] args) throws IOException {
-        System.out.println(" .----------------.  .----------------.  .----------------.  .----------------. ");
-        System.out.println("| .--------------. || .--------------. || .--------------. || .--------------. |");
-        System.out.println("| | _____  _____ | || |     ____     | || |  _______     | || |  ___  ____   | |");
-        System.out.println("| ||_   _||_   _|| || |   .'    `.   | || | |_   __ \\    | || | |_  ||_  _|  | |");
-        System.out.println("| |  | | /\\ | |  | || |  /  .--.  \\  | || |   | |__) |   | || |   | |_/ /    | |");
-        System.out.println("| |  | |/  \\| |  | || |  | |    | |  | || |   |  __ /    | || |   |  __'.    | |");
-        System.out.println("| |  |   /\\   |  | || |  \\  `--'  /  | || |  _| |  \\ \\_  | || |  _| |  \\ \\_  | |");
-        System.out.println("| |  |__/  \\__|  | || |   `.____.'   | || | |____| |___| | || | |____||____| | |");
-        System.out.println("| |              | || |              | || |              | || |              | |");
-        System.out.println("| '--------------' || '--------------' || '--------------' || '--------------' |");
-        System.out.println(" '----------------'  '----------------'  '----------------'  '----------------' ");
+        createDirectory();
+
 
         var scanner = new Scanner(System.in);
-        File file = new File(directory);
-        if (!file.exists()) {
-            file.mkdirs();
-        }
 
         FileWriter myWriter = new FileWriter(directory + fileName, true);
 
         System.out.println(" ");
         System.out.println("How many hour's did You worked today?");
+
         int time = scanner.nextInt();
         if (time == 1) {
             h = String.valueOf(Hours.HOUR).toLowerCase();
@@ -69,7 +60,9 @@ public class WorkTimerApplication {
         while (reader.readLine() != null) lines++;
         reader.close();
 
-        if (lines == YearMonth.now().lengthOfMonth()) {
+        if (lines == YearMonth.now().
+
+                lengthOfMonth()) {
             myWriter.write("Work time summary of " + month + " " + sum);
             myWriter.write("\n");
             myWriter.write("Salary for september: " + sum * salaryPerHour + " PLN");
@@ -77,44 +70,16 @@ public class WorkTimerApplication {
         }
 
     }
-/*
-    public class TimerButtons implements ActionListener {
-        JFrame frame;
-        JTextField textField;
-        JButton[] functionButtons = new JButton[3];
-        JButton addButton, sumButton;
-        JPanel panel;
 
-        Font myFont = new Font("Times New Roman", Font.PLAIN, 15);
+    private static void createDirectory() {
+        File file = new File(directory);
+        if (!file.exists()) {
+            file.mkdirs();
 
-        TimerButtons() {
-            frame = new JFrame("Work Timer");
-            frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-            frame.setSize(420, 550);
-            frame.setLayout(null);
-            frame.getColorModel();
-
-            textField = new JTextField();
-            textField.setBounds(50, 25, 300, 50);
-            textField.setFont(myFont);
-            textField.setEditable(false);
-
-            addButton = new JButton("Add hours of work");
-            panel.add(addButton);
-            addButton.addActionListener(new ActionListener() {
-                @Override
-                public void actionPerformed(ActionEvent e) {
-                    JOptionPane.getFrameForComponent(addButton);
-
-                }
-            });
 
         }
-
-        @Override
-        public void actionPerformed(ActionEvent e) {
-
-        }
-    }*/
-
+    }
 }
+
+
+
